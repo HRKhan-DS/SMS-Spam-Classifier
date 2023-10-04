4911c3e748b1c19ea2ec7944933f4948b6f10cbe
Md Harun-Or-Rashid Khan <141298348+HRKhan-DS@users.noreply.github.com>
2023-10-05 02:05:42 +0600

    app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.title("SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
-if st.button('Predict'):
+if st.button('Classify'):
 
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
@@ -57,3 +57,13 @@ if st.button('Predict'):
     else:
         st.header("Not Spam Message")
         st.write("This message is not considered spam.")
+
+
+# Larger gap using multiple <br> tags
+st.markdown("<br><br><br>", unsafe_allow_html=True)
+
+# Instructions for SMS Spam Classifier
+st.write("Welcome to the SMS Spam Classifier!")
+st.write("To determine if a message is spam or not, please enter the SMS text in the input box.")
+st.write("Click the 'Classify' button, and the result (spam or not spam) will be displayed above.")
+
