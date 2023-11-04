002c99d3c6f3813cd1aed3fb55c948e4ab465a85
Md Harun-Or-Rashid Khan <141298348+HRKhan-DS@users.noreply.github.com>
2023-11-04 23:43:52 +0600

    app.py

--- app.py
+++ app.py
@@ -1,69 +1,45 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 import pickle
-import string
-from nltk.corpus import stopwords
-import nltk
-from nltk.stem.porter import PorterStemmer
-
-nltk.download('punkt')
-nltk.download('stopwords')
-
-ps = PorterStemmer()
 
 
-def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
+with open("SVCModel.pkl", "rb") as model_file:
+    svc_model = pickle.load(model_file)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
+df = pd.read_csv('new_data.csv')
 
-    text = y[:]
-    y.clear()
+def main():
+    st.title("Fake SMS Classification")
 
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
+    # Create an input text area for the user to enter news content
+    input_data = st.text_area("Enter the SMS text:")
 
-    text = y[:]
-    y.clear()
+    if st.button("Classify"):
+        # Make sure the input data is not empty
+        if input_data:
+            # Perform any preprocessing on input_data if necessary (e.g., vectorization)
+            # Here, you should apply the same preprocessing steps used when training the model
 
-    for i in text:
-        y.append(ps.stem(i))
+            # Predict the class of the news
+            prediction = svc_model.predict([input_data])
 
-    return " ".join(y)
+            # Display the prediction result
+            if prediction[0] == 0:
+                st.write('The SMS is classified as Real')
+            else:
+                st.write('The SMS is classified as Fake')
+        else:
+            st.write("Please enter some SMS text before classifying.")
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+    # Larger gap using multiple <br> tags
+    st.markdown("<br><br><br>", unsafe_allow_html=True)
 
-st.title("SMS Spam Classifier")
+    # Instructions for SMS Spam Classifier
+    st.write("Welcome to the Fake SMS Classifier!")
+    st.write("To determine whether the SMS is true or fake, enter some text into the box.")
+    st.write("Click the 'Classify' button, and the result (Real or fake) will be displayed above.")
 
-input_sms = st.text_area("Enter the message")
-
-if st.button('Classify'):
-
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam Message")
-        st.write("This message has been classified as spam.")
-    else:
-        st.header("Not Spam Message")
-        st.write("This message is not considered spam.")
-
-
-# Larger gap using multiple <br> tags
-st.markdown("<br><br><br>", unsafe_allow_html=True)
-
-# Instructions for SMS Spam Classifier
-st.write("Welcome to the SMS Spam Classifier!")
-st.write("To determine if a message is spam or not, please enter the SMS text in the input box.")
-st.write("Click the 'Classify' button, and the result (spam or not spam) will be displayed above.")
+if __name__ == '__main__':
+    main()
 
